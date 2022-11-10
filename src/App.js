4a47e54adb8bab0a6fbc26f04f26b4412a15bc4f
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-11-10 10:48:03 -0500

    Styled to make Categories readable.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import axios from 'axios';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(()=>{
+    axios.get(`https://opentdb.com/api_category.php`)
+      .then(res => {
+         const newCategories = res.data.trivia_categories;
+
+        setCategories(newCategories); 
+      });
+  }, []);
 
   return (
     <div className="App">
-        <header className="title">React Trivia</header>
-        <section>
-
-        </section>
+        <header className="title"><h1>Trivia</h1></header>
+        <ul>
+          {categories.map(result=> (
+            <li key={result.id}>{result.name}</li>
+          ))}
+        </ul>
     </div>
   );
 }
